0f0b95cc08c182ea1f30934d561ccf80290671fc
tam11a <ibrahimsadiktamim@gmail.com>
2024-12-08 02:31:58 +0600

    theme setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,41 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
+const outfit = Outfit({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Next.js Shadcn Starter",
+	description: "A starter for Next.js with Tailwind CSS and TypeScript.",
+	icons: "/favicon.svg",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={cn(outfit.className, "bg-background")}
+				suppressHydrationWarning
+			>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+					<div className="fixed bottom-5 right-5">
+						<ModeToggle />
+					</div>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }

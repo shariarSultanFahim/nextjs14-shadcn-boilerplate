38ae345841559f4d556e4109744acebc39d8fa8e
Shariar Sultan Fahim <157113723+shariarSultanFahim@users.noreply.github.com>
2024-12-08 21:32:40 +0600

    users page for admin route added

--- app/(private)/(admin)/users/(list)/page.tsx
+++ app/(private)/(admin)/users/(list)/page.tsx
@@ -1,7 +1,23 @@
+import { Separator } from "@/components/ui/separator";
+import { CreateUser } from "./create-users";
+
 export default function UsersListPage() {
 	return (
-		<div>
-			<h1>Users List Page</h1>
-		</div>
+		<div className="space-y-4 block">
+      <div className="p-6 pb-1 flex flex-row items-center justify-between">
+        <div className="space-y-0.5 ">
+          <h2 className="text-2xl font-bold tracking-tight">Users</h2>
+          <p className="text-muted-foreground text-sm">
+            Here&apos;s a list of your employees!
+          </p>
+        </div>
+        <CreateUser/>
+      </div>
+      <Separator />
+
+      {/* <div className="px-9">
+        <UserTable />
+      </div> */}
+    </div>
 	);
 }

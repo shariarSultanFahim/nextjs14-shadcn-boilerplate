30183201f36bd804730a6b0a1466b8c5e0b8e687
Jannat <shahrinasabrin@gmail.com>
2024-12-09 08:02:26 +0600

    added course-form

--- app/(private)/(admin)/admin-sidebar.tsx
+++ app/(private)/(admin)/admin-sidebar.tsx
@@ -1,57 +1,62 @@
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarGroup,
-	SidebarGroupContent,
-	SidebarGroupLabel,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { Home, Settings, Users2 } from "lucide-react";
+import { GraduationCap, Home, Settings, Users2 } from "lucide-react";
 import Link from "next/link";
 
 // Menu items.
 const items = [
-	{
-		title: "Home",
-		url: "/dashboard",
-		icon: Home,
-	},
-	{
-		title: "Users",
-		url: "/users",
-		icon: Users2,
-	},
-	{
-		title: "Settings",
-		url: "#",
-		icon: Settings,
-	},
+  {
+    title: "Home",
+    url: "/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Users",
+    url: "/users",
+    icon: Users2,
+  },
+  {
+    title: "Courses",
+    url: "/courses",
+    icon: GraduationCap,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
 ];
 
 export function AdminSidebar() {
-	return (
-		<Sidebar>
-			<SidebarContent>
-				<SidebarGroup>
-					<SidebarGroupLabel>Application</SidebarGroupLabel>
-					<SidebarGroupContent>
-						<SidebarMenu>
-							{items.map((item) => (
-								<SidebarMenuItem key={item.title}>
-									<SidebarMenuButton asChild>
-										<Link href={item.url}>
-											<item.icon />
-											<span>{item.title}</span>
-										</Link>
-									</SidebarMenuButton>
-								</SidebarMenuItem>
-							))}
-						</SidebarMenu>
-					</SidebarGroupContent>
-				</SidebarGroup>
-			</SidebarContent>
-		</Sidebar>
-	);
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
 }
